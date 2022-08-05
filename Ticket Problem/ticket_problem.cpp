dfa7d6aa558e3da6aa69f37137017645a3219a23
Ashim Dongol <85755127+AshimD1@users.noreply.github.com>
2022-08-06 00:54:34 +0530

    Add files via upload

---
+++ Ticket Problem/ticket_problem.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int ages[5];
+
+    for (int i = 0; i < 5; ++i) {
+        cin >> ages[i];
+    }
+    //your code goes here
+    float money;
+   
+    int &min = *min_element(ages,ages+5 );
+
+    money = 50 - 0.01*50*min;
+    cout<<money;
+    
+    
+    return 0;
+}
